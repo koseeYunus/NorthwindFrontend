7cea661bdfa066ec86674c7fa9921e673b554688
Yunus Köse <kose.yunus.55@gmail.com>
2021-03-22 18:02:05 +0300

    Product-add component added.

--- src/app/components/product-add/product-add.component.ts
+++ src/app/components/product-add/product-add.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {FormGroup, FormBuilder, FormControl, Validators} from "@angular/forms"
+import { ToastrService } from 'ngx-toastr';
+import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-product-add',
@@ -10,7 +12,7 @@ export class ProductAddComponent implements OnInit {
 
   productAddForm: FormGroup;
 
-  constructor(private formBuilder: FormBuilder) { }
+  constructor(private formBuilder: FormBuilder, private productService:ProductService, private toastrService:ToastrService) { }
 
   ngOnInit(): void {
     this.createProductAddForm();
@@ -23,6 +25,26 @@ export class ProductAddComponent implements OnInit {
       unitPrice: ["", Validators.required],
       unitsInStock: ["", Validators.required]
     })
+  }
+
+  add(){
+    if(this.productAddForm.valid){
+      let productModel= Object.assign({}, this.productAddForm.value)
+      this.productService.add(productModel).subscribe(response=>{
+        this.toastrService.success(response.message,"Başarılı")
+      },responseError=>{
+        if(responseError.error.Errors.length>0){
+          for (let i = 0; i < responseError.error.Errors.length; i++) {
+            this.toastrService.error(responseError.error.Errors[i].ErrorMessage, "Doğrulama hatası")
+          }
+          
+        }
+        
+      })     
+    }
+    else{
+      this.toastrService.error("Forma girdiğiniz değer hatalı","Dikkat")
+    }
   }
 
 }
